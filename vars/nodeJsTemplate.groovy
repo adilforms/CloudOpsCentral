85d64e9e6e04eccb309788c0cefeb4d8c95fd49f
Adil <adilforms@gmail.com>
2019-01-03 14:27:14 -0500

    removed echo

--- vars/nodeJsTemplate.groovy
+++ vars/nodeJsTemplate.groovy
@@ -1,6 +1,6 @@
 def dockerImageName() {
-  def imageName = sh(returnStdout: true, script: """#!/bin/bash -el
-      version=$(echo git rev-parse --short HEAD)
+  def imageName = sh(returnStdout: true, script: """#!/bin/bash -l
+      version=$(git rev-parse --short HEAD)
       PName=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
       PackageName=$PName
       echo "$PackageName.$version:$BRANCH_NAME"
